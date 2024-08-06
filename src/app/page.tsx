46e5368dc79343796b9f7366b4226b8310c0eb53
jiminsample1 <jiminsample1@gmail.com>
2024-08-07 02:02:14 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,15 @@
 import { useState } from "react";
 import { useDraw } from "../hooks/useDraw";
 import { ChromePicker } from "react-color";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3001");
+
 export default function Home() {
   const [color, setColor] = useState<string>("#000");
   const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
 
-  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
-    const { x: currX, y: currY } = currentPoint;
-    const lineColor = color;
-    const lineWidth = 5;
-
-    let startPoint = prevPoint ?? currentPoint;
-    ctx.beginPath();
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = lineColor;
-    ctx.moveTo(startPoint.x, startPoint.y);
-    ctx.lineTo(currX, currY);
-    ctx.stroke();
-
-    ctx.fillStyle = lineColor;
-    ctx.beginPath();
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {}
+  function createLine({ prevPoint, currentPoint, ctx }: Draw) {}
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
       <div className="flex flex-col gap-10 pr-10">
